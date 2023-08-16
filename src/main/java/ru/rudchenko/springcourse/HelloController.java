c396874868e7e560ae6ba57891508f0c74202f46
Alexandr <sasharudchenko@yandex.ru>
2023-08-17 01:37:07 +0300

    commit

---
+++ src/main/java/ru/rudchenko/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package ru.rudchenko.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
